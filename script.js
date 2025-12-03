387f0dac876f57764781f25a5801e7e555483dea
Martin Lutherking owino <programingtmaster@hotmail.com>
2025-12-03 12:32:51 +0300

    Refactor product list and improve cart functionality

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ const products = [
   { id:3, name:"west african dress", category:"Ankara", price:7500, image:"images/martin 3.jpg", rating:4 },
   { id:4, name:"kids west african shirt", category:"Ankara", price:1500, image:"images/martin 2.jpg", rating:3 },
   { id:5, name:"west african kitenge", category:"Casual", price:4500, image:"images/mama 2.jpg", rating:4 },
-  { id:6, name:"Printed Tee", category:"Casual", price:7500, image:"images/mama 1.jpg", rating:3 },
+  { id:6, name:"Dress", category:"dress", price:7500, image:"images/mama 1.jpg", rating:3 },
   { id:7, name:"Blue Agbada Jacket", category:"Agbada", price:2500, image:"images/dad 50.jpg", rating:5 },
   { id:8, name:"baubaus", category:"Baubaus", price:4500, image:"images/dera 1.jpg", rating:4 },
   { id:9, name:"Blouse", category:"Blouse", price:7500, image:"images/Heco1.2.jpg", rating:4 },
@@ -158,6 +158,7 @@ function setupMobileMenu() {
 // Expose functions
 window.addToCart = addToCart;
 window.removeFromCart = removeFromCart;
+
 
 
 
